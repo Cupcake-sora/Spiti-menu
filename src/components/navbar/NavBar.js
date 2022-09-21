21468f1010f3e020116582470c877768b7b3fbae
Ana <pereirarojasanab@gmail.com>
2022-09-21 12:13:07 -0300

    menu e-commerce

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { MdOutlineCoffeeMaker } from 'react-icons/md';
+
+function NavBar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#"><MdOutlineCoffeeMaker/>spiti</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Dulce</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Salado</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Bebidas</a>
+                        </li>
+                    </ul>
+                    <form className="d-flex">
+                        <button className="btn btn-outline-success" type="submit">Carrito</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
